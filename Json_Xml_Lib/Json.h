90b53e501f447b787dddfb766af601aed6549540
NSH <nsh4842@gmail.com>
2025-07-24 21:10:19 +0900

    구조 다시 설계 소멸자 설계를 제대로 해야함

--- Json_Xml_Lib/Json.h
+++ Json_Xml_Lib/Json.h
@@ -8,5 +8,32 @@ using namespace std;
 using namespace Dynamic;
 
 namespace JSON {
+	class JNode {
+		enum class JType {
+			OBJ,
+			ARR,
+			NUMBER,
+			DOUBLE,
+			BOOL,
+			STRING,
+			CHAR,
+			Null	//³Î
+		};
+	};
 
+	class JObj {
+
+	};
+
+	class JArr {
+
+	};
+
+	class JsonCtrl {
+
+	};
+
+	class JsonData {
+
+	};
 }
